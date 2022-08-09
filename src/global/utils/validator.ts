544e35e892a0b984c9ae2692e849b94189e386ac
Matheus Reis <matheus.reis081@gmail.com>
2022-08-09 15:59:57 -0300

    feat: create Validator class, and methods isEmailValid and isPasswordValid

---
+++ src/global/utils/validator.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-useless-escape
+const emailRegex = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+const validatorEmail = new RegExp(emailRegex);
+
+export class Validator {
+  static isEmailValid (email:string) {
+    return validatorEmail.test(email);
+  }
+
+  static isPasswordValid (password: string) {
+    if (password.length > 8) { return true; }
+    return false;
+  }
+}
