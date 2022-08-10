b2565c6d68b9fb54a73170f6ef1853b43a17ae2f
Matheus Reis <matheus.reis081@gmail.com>
2022-08-10 17:45:54 -0300

    test: ensure that signInUseCase call authRepository with correct params

--- __test__/auth/domain/useCases/signInUseCase.spec.ts
+++ __test__/auth/domain/useCases/signInUseCase.spec.ts
@@ -1,6 +1,20 @@
 
 import { EmptyParamFieldError } from '../../../../src/auth/domain/errors/EmptyParamFieldError';
+import { IAuthRepository } from '../../../../src/auth/domain/irepositories/authRepository';
+import { SignInResponseDTO } from '../../../../src/auth/presentation/dtos/SignInResponseDTO';
+
+// class AuthRepository implements IAuthRepository {
+//   signIn (email: string, password: string): Promise<SignInResponseDTO> {
+//     return null;
+//   };
+
+//   signUp (): void {
+//     throw new Error('Method not implemented.');
+//   }
+// }
 class SignInUseCase {
+  constructor (private authRepository: IAuthRepository) {}
+
   async execute (email: string, password: string) {
     if (!email) {
       throw new EmptyParamFieldError('email');
@@ -8,13 +22,19 @@ class SignInUseCase {
     if (!password) {
       throw new EmptyParamFieldError('password');
     }
+
+    await this.authRepository.signIn(email, password);
   }
 }
 
 const makeSut = () => {
-  const sut = new SignInUseCase();
+  const repository = {
+    signIn: jest.fn(),
+    signUp: jest.fn()
+  };
+  const sut = new SignInUseCase(repository);
 
-  return { sut };
+  return { sut, repository };
 };
 
 describe('SignInUseCase', () => {
@@ -27,5 +47,15 @@ describe('SignInUseCase', () => {
     const { sut } = makeSut();
 
     expect(async () => await sut.execute('matheus.reis@gmail.com', '')).rejects.toThrow(new EmptyParamFieldError('password'));
+  });
+  it('should repository receive email and password correctly', async () => {
+    const email = 'email.correct@gmail.com';
+    const password = '123*456*78';
+
+    const { sut, repository } = makeSut();
+
+    await sut.execute(email, password);
+
+    expect(repository.signIn).toBeCalledWith(email, password);
   });
 });
