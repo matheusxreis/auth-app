db74375662a4cf0b06d8f02fcf33581331d36bc2
Matheus Reis <matheus.reis081@gmail.com>
2022-08-14 10:41:03 -0300

    test: ensure that signUpUseCase throw if params is empty

---
+++ __test__/auth/data/useCases/signUpUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { EmptyParamFieldError } from '../../../../src/global/errors/EmptyParamFieldError';
+
+interface ISignUpExecuteParams {
+username: string,
+password: string,
+email:string
+}
+
+export class SignUpUseCase {
+  async execute (params: ISignUpExecuteParams) {
+    if (!params.email) { throw new EmptyParamFieldError('email'); }
+    if (!params.password) { throw new EmptyParamFieldError('password'); }
+    if (!params.username) { throw new EmptyParamFieldError('username'); }
+  }
+}
+
+const makeSut = () => {
+  const sut = new SignUpUseCase();
+
+  return { sut };
+};
+
+// const params = { username: 'Username', email: 'valid.email@gmail.com', password: '1223h382' };
+describe('SignUpUseCase', () => {
+  it('should throw if email is empty', async () => {
+    const { sut } = makeSut();
+    const errorParams = { username: 'Username', email: '', password: '1223h382' };
+    expect(async () => await sut.execute(errorParams)).rejects.toThrow(new EmptyParamFieldError('email'));
+  });
+  it('should throw if password is empty', async () => {
+    const { sut } = makeSut();
+    const errorParams = { username: 'Username', email: 'valid.email@gmail.com', password: '' };
+    expect(async () => await sut.execute(errorParams)).rejects.toThrow(new EmptyParamFieldError('password'));
+  });
+  it('should throw if username is empty', async () => {
+    const { sut } = makeSut();
+    const errorParams = { username: '', email: 'valid.email@gmail.com', password: '1223h382' };
+    expect(async () => await sut.execute(errorParams)).rejects.toThrow(new EmptyParamFieldError('username'));
+  });
+});
